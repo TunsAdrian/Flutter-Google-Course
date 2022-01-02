ce9a2f52df8c0cda2a7b00ec72afdcb7beac1acb
Adrian-Ioan Tuns <50252048+TunsAdrian@users.noreply.github.com>
2022-01-02 17:01:29 +0200

    structure refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 
-import 'lab4/CountriesAndFlags.dart';
+import 'countries_and_flags//CountriesAndFlags.dart';
 
 void main() {
   // Switch the above import with the file containing a 'MyApp' that is desired to be run
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:google_hw/lab4/CountriesAndFlags.dart';
+import 'package:google_hw/countries_and_flags/CountriesAndFlags.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
